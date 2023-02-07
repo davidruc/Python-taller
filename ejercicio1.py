cefbc50d8c03c9d4b84b152202c3fb119a7341a1
DavidAndrésRuedaChacónCAMPUS <davidarueda18@gmail.com>
2023-02-07 11:12:22 -0500

    Solucion ejercicio 1

---
+++ ejercicio1.py
@@ -0,0 +1,20 @@
+"""
+1. Qué operadores utiliza Python en los siguientes casos:
+
+A. División Modular
+B. Exponenciación
+C. División que retorne entero.
+"""
+
+ 
+a = 2
+b = 52
+print(f" B. exponenciación {a}**{b} = {a**b}")
+
+a = 500
+b = 256
+print(f" A.División modular {a} % {b} = {a%b}")
+print(f" C. División que retorne entero {a} // {b} = {a//b}") 
+
+
+
